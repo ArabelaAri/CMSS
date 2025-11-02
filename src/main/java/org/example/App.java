09234a5a675704ad0f634e5a7d0ff0ae5242e976
Arabela <adelka.kozinova@gmail.com>
2025-11-02 21:25:39 +0100

    init commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,28 +3,110 @@ package org.example;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+    public void start(Stage primaryStage) {
+        // Buttons
+        ToggleButton drawButton = new ToggleButton("Draw");
+        ToggleButton rubberButton = new ToggleButton("Rubber");
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+        ToggleButton[] toolsArr = {drawButton, rubberButton};
+        ToggleGroup tools = new ToggleGroup();
+
+        for (ToggleButton tool : toolsArr) {
+            tool.setMinWidth(90);
+            tool.setToggleGroup(tools);
+            tool.setCursor(Cursor.HAND);
+        }
+
+        ColorPicker cpLine = new ColorPicker(Color.BLACK);
+
+        Slider slider = new Slider(1, 50, 3);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+
+        Label line_color = new Label("Line Color");
+        Label line_width = new Label("3.0");
+
+        VBox buttons = new VBox(10);
+        buttons.getChildren().addAll(drawButton, rubberButton, line_color, cpLine, line_width, slider);
+        buttons.setPadding(new Insets(5));
+        buttons.setStyle("-fx-background-color: #999");
+        buttons.setPrefWidth(100);
+
+        // Canvas
+        Canvas canvas = new Canvas(1080, 790);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(1);
+
+        canvas.setOnMousePressed(e -> {
+            if (drawButton.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.beginPath();
+                gc.lineTo(e.getX(), e.getY());
+            } else if (rubberButton.isSelected()) {
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+        });
+
+        canvas.setOnMouseDragged(e -> {
+            if (drawButton.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+            } else if (rubberButton.isSelected()) {
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+        });
+
+        canvas.setOnMouseReleased(e -> {
+            if (drawButton.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+                gc.closePath();
+            } else if (rubberButton.isSelected()) {
+                double lineWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - lineWidth / 2, e.getY() - lineWidth / 2, lineWidth, lineWidth);
+            }
+        });
+
+        // Color pickers
+        cpLine.setOnAction(e -> gc.setStroke(cpLine.getValue()));
+
+        // Slider
+        slider.valueProperty().addListener(e -> {
+            double width = slider.getValue();
+            line_width.setText(String.format("%.1f", width));
+            gc.setLineWidth(width);
+        });
+        // Layout
+        BorderPane pane = new BorderPane();
+        pane.setRight(buttons);
+        pane.setCenter(canvas);
+
+
+        Scene scene = new Scene(pane, 1200, 800);
+        primaryStage.setTitle("Paint");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
